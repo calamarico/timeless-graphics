2656fe5fbf87035600aa70b1b6d55107aba6b7cc
Daniel Hernández <daniel.hernandez.zafra@gmail.com>
2026-04-27 04:25:27 +0200

    Add eastern egg

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+console.log(
+  `%c Designed by:\n` +
+  `%c` +
+  ` ██╗  ██╗ █████╗ ██╗      █████╗ ███╗   ███╗ █████╗ ██████╗ ██╗ ██████╗ ██████╗ \n` +
+  ` ██║ ██╔╝██╔══██╗██║     ██╔══██╗████╗ ████║██╔══██╗██╔══██╗██║██╔════╝██╔═══██╗\n` +
+  ` █████╔╝ ███████║██║     ███████║██╔████╔██║███████║██████╔╝██║██║     ██║   ██║\n` +
+  ` ██╔═██╗ ██╔══██║██║     ██╔══██║██║╚██╔╝██║██╔══██║██╔══██╗██║██║     ██║   ██║\n` +
+  ` ██║  ██╗██║  ██║███████╗██║  ██║██║ ╚═╝ ██║██║  ██║██║  ██║██║╚██████╗╚██████╔╝\n` +
+  ` ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═════╝ \n\n` +
+  ` 🔗 @calamarico\n` +
+  ` 🌐 https://www.kalamarico.lol\n`,
+  'color: #aaa; font-size: 14px; font-weight: normal;',
+  'color: #e0a0ff; font-size: 10px; font-family: monospace; font-weight: bold;'
+)
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
